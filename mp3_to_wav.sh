3912049a449df46e7d9d044949dbe469e3aed41b
Vadym Rudenko <vadym.rudenko@gmail.com>
2014-06-01 16:34:05 -0700

    improvements

--- mp3_to_wav.sh
+++ mp3_to_wav.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-DIR=/Volumes/WD1T/music_to_go/jazz
+[ $# -eq 1 ] || { echo "Usage: $0 <inputdir>"; exit 1; }
+
+INPUT_DIR="$1"
+OUTPUT_DIR_NAME=$(basename "${INPUT_DIR}")
+OUTPUT_STORAGE=/Volumes/WD1T/tests/
+
+#TRACKLIST=$(${OUTPUT_STORAGE}/${OUTPUT_DIR}/tracklist.txt)
+#mkdir /Volumes/WD1T/tests/${OUTPUT_DIR}
+
+[ -d ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME} ] || mkdir ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME}
 
 ffmpeg=/usr/local/bin/ffmpeg
 
-#ls $DIR > tracklist.txt
+ls ${INPUT_DIR}/ >> ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME}/tracklist.txt
 
-#sed 's/ //g' _tracklist.txt >> tracklist.txt
+#TODO:
+#   done: create new folder for output files
+#   done: substitute harcoded input dir
+#   avoid white spaces in directory names
+#   avoid white spaces in file names
+#   make variable for tracklist
 
-cat jazz | while read i; do
-   # track=$( echo $i | awk -F".mp3" '{print $1}' )
-    track=$i
+cat ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME}/tracklist.txt | while read i; do
+    track=$( echo $i | awk -F".mp3" '{print $1}' )
+   # track=$i
     echo ${track}
-    $ffmpeg -i ${DIR}/${track}.mp3 ${DIR}/${track}.wav
+    $ffmpeg -i ${INPUT_DIR}/${track}.mp3 ${OUTPUT_DIR}/${track}.wav
     echo 'next';
 done
