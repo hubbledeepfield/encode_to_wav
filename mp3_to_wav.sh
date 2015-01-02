9ae4caeabd66c7d97a3030707d431bafbcf93035
Vadym Rudenko <vadym.rudenko@gmail.com>
2015-01-01 21:39:36 -0800

    delete original

--- mp3_to_wav.sh
+++ mp3_to_wav.sh
@@ -7,7 +7,7 @@ DIR_NAME=$(basename "${INPUT_DIR}")
 OUTPUT_STORAGE=/Volumes/WD1T/tests/
 
 #TRACKLIST=$(${OUTPUT_STORAGE}/${OUTPUT_DIR}/tracklist.txt)
-#mkdir /Volumes/WD1T/tests/${OUTPUT_DIR}
+#mkdir ${OUTPUT_STORAGE}/${OUTPUT_DIR}
 
 #TODO:
 #   done: create new folder for output files
@@ -17,7 +17,7 @@ OUTPUT_STORAGE=/Volumes/WD1T/tests/
 #   make variable for tracklist
 #   delete non-media files
 
-#[ -d ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME} ] || mkdir ${OUTPUT_STORAGE}/${OUTPUT_DIR_NAME}
+[ -d ${OUTPUT_STORAGE}/${DIR_NAME} ] || mkdir ${OUTPUT_STORAGE}/${DIR_NAME}
 
 ffmpeg=/usr/local/bin/ffmpeg
 
@@ -30,12 +30,11 @@ cat tracklist.txt | while read i; do
     mv "$i" "${i//[[:space:]]}"
 done
 
-rm tracklist.txt
-
 ls >> tracks_to_encode.txt
 cat tracks_to_encode.txt | while read i; do \
     track=$( echo $i | awk -F".mp3" '{print $1}' )
     $ffmpeg -i $track.mp3 $track.wav
     echo $track
+    rm $track.mp3
 done;
 
