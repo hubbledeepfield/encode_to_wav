60986aa943179029913038f2ee4c229996006163
Vadym Rudenko <vadym.rudenko@gmail.com>
2015-01-10 23:03:15 -0800

    working on more supported formats

--- mp3_to_wav.sh
+++ mp3_to_wav.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-[ $# -eq 1 ] || { echo "Usage: $0 <inputdir>"; exit 1; }
+[ $# -eq 2 ] || { echo "Usage: $0 <inputdir>, <dest_format>"; exit 1; }
 
 INPUT_DIR="$1"
+dest_format="$2"
 DIR_NAME=$(basename "${INPUT_DIR}")
 OUTPUT_STORAGE=/Volumes/WD1T/tests/
 
@@ -20,21 +21,31 @@ OUTPUT_STORAGE=/Volumes/WD1T/tests/
 [ -d ${OUTPUT_STORAGE}/${DIR_NAME} ] || mkdir ${OUTPUT_STORAGE}/${DIR_NAME}
 
 ffmpeg=/usr/local/bin/ffmpeg
+acodec=pcm_s16le
+
 
 cp -r ${INPUT_DIR} ${OUTPUT_STORAGE}
 
 cd ${OUTPUT_STORAGE}/${DIR_NAME}
 
-ls >> tracklist.txt
+ls  > tracklist.txt
 cat tracklist.txt | while read i; do
     mv "$i" "${i//[[:space:]]}"
 done
 
-ls >> tracks_to_encode.txt
-cat tracks_to_encode.txt | while read i; do \
-    track=$( echo $i | awk -F".mp3" '{print $1}' )
-    $ffmpeg -i $track.mp3 $track.wav
-    echo $track
-    rm $track.mp3
+if [ $dest_format == "mp3" ]; then
+    for track in $INPUT_DIR
+    do
+        echo $track
+        #$ffmpeg -y -i $track -codec:a libmp3lame -qscale:a 0 $track.mp3
+    done
+fi
+
+ls | grep "mp3" > tracks_to_encode.txt
+cat tracks_to_encode.txt | while read i; do
+    title=$( echo $i | awk -F".mp3" '{print $1}' )
+    $ffmpeg -y -i $title.mp3 -acodec $acodec $title.wav
+    echo $title
+    rm $title.mp3
 done;
 
